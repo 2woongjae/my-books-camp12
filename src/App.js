2a49dff3aa057876465313749e1860276c1bc2d3
Mark Lee <mark@protopie.io>
2020-08-01 14:02:37 +0900

    week4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import NotFound from './pages/NotFound';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary Error={Error}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/signin" component={Signin} />
+          <Route path="/" exact component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
