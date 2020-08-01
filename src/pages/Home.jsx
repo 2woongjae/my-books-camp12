be9b2d005f9aa5fd654e05fa2e3af3b9ba65efdc
Mark Lee <mark@protopie.io>
2020-08-01 15:21:40 +0900

    get books

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import withAuth from '../hocs/withAuth';
+import axios from 'axios';
 
-export default function Home() {
-  const token = localStorage.getItem('token');
-  if (token === null) {
-    return <Redirect to="/signin" />;
+class Home extends React.Component {
+  state = {
+    books: [],
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>{this.props.token}</p>
+        <ul>
+          {this.state.books.map((book) => (
+            <li>{book}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+
+  async componentDidMount() {
+    // 책 가져오기
+    try {
+      const response = await axios.get('https://api.marktube.tv/v1/book', {
+        headers: {
+          Authorization: `Bearer ${this.props.token}`,
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+    // this.setState({ books: ['hello', 'world'] });
+  }
 }
+
+export default withAuth(Home);
