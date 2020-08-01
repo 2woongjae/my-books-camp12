1704b83950f9ca20af317dfff8ac86903fdb9266
Mark Lee <mark@protopie.io>
2020-08-01 14:55:39 +0900

    week4-1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 export default function Home() {
+  const token = localStorage.getItem('token');
+  if (token === null) {
+    return <Redirect to="/signin" />;
+  }
   return (
     <div>
       <h1>Home</h1>
